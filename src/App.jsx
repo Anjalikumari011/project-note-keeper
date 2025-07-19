735ffdb0e123a0b588fd9960cf41002790934d6f
Anjalikumari011 <anjalishah.as11@gmail.com>
2025-07-19 17:00:07 +0530

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,52 @@
-import{BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import AddForm from "./pages/AddForm";
 import NoteDetails from "./pages/NoteDetails";
-import EditFrom from "./pages/EditForm";
+import EditForm from "./pages/EditForm";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Head from "./components/Head";
-import { useAuth} from "./context/AuthContext";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
-    const {user, loading}= useAuth();
+  const { user, loading } = useAuth();
 
-    if(loading){
-        return <div className="flex justify-center items-centers h-screen">Loading...</div>;
-    }
-     
-    return (
-        <Router>
-            <Head/>
-            <Routes>
-                <Route 
-                path ="/login"
-                element={!user ?<Login/> : <Navigate to="/dashboard" replace />}
-                />
-                <Route
-                path="/signup"
-                element={!user ?<Signup/>: <Navigate to="/dashboard" replace />}
-                />
-                <Route
-                path="/dashboard"
-                element={user ? <Home/> :< Navigate to ="/login" replace/>}
-                />
-                <Route
-                path="/add"
-                element={user ? <AddForm/>: <Navigate to="/login" replace/>}
-                />
-                <route
-                path="/details/:id"
-                element={user? <NoteDetails/>: <Navigate to="/login" replace/>}
-                />
-                <route
-                path="/edit/:id"
-                element={user ? <EditFrom/> :<Navigate to="/login" replace/>}
-                />
-                <route
-                path="*"
-                element={<Navigate to={user ? "/dashboard": "login"} replace/>}
-                />
-            </Routes>
-            </Router>
-    );
+  if (loading) {
+    return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  }
+
+  return (
+    <Router>
+      <Head />
+      <Routes>
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/dashboard" replace />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/dashboard" replace />}
+        />
+        <Route
+          path="/dashboard"
+          element={user ? <Home /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/add"
+          element={user ? <AddForm /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/details/:id"
+          element={user ? <NoteDetails /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/edit/:id"
+          element={user ? <EditForm /> : <Navigate to="/login" replace />}
+        />
+        <Route path="*" element={<Navigate to={user ? "/dashboard" : "/login"} replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
