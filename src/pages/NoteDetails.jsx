28f3e2ed5ede72d4be73aecaff392548e70e5909
Anjalikumari011 <anjalishah.as11@gmail.com>
2025-07-22 21:13:11 +0530

    note details

--- src/pages/NoteDetails.jsx
+++ src/pages/NoteDetails.jsx
@@ -25,7 +25,7 @@ export default function NoteDetails(){
 
     return(
         <div className="min-h-screen bg-gradient-to-br from-indigo-800 via-purple-700 to-pink-700 flex items-center justify-center px-4 py-12">
-            <div className="max-w-3xl w-full bd-white/90 backdrop-blur-md rounded-3xl shadow-2xl p-8">
+            <div className="max-w-3xl w-full bg-white/90 backdrop-blur-md rounded-3xl shadow-2xl p-8">
                 <button
                 onClick={()=>navigate('/dashboard')}
                 className="w-[100%] text-indigo-700 font-medium rounded-lg text-sm px-5 py-2.5 text-left transition cursor-pointer -mt-[10px]">
